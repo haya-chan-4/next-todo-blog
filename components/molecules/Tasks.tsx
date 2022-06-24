cc9f28ddb35cb049e2a863d25829127a66b2959e
Hayata <hayata8812@gmail.com>
2022-06-24 10:55:59 +0900

    変更

--- components/molecules/Tasks.tsx
+++ components/molecules/Tasks.tsx
@@ -3,22 +3,22 @@ import { Task } from '../atoms/Task';
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 
-// type Props = {
-//   inputText: string,
-//   taskList: any,
-//   setTaskList: any
-// }
+type Props = {
+  inputText: string,
+  taskList: any,
+  setTaskList: any
+}
 
-const reorder = (taskList, startIndex, endIndex) => {
+const reorder = (taskList: any[], startIndex: any, endIndex: any) => {
   // タスクの並び替え
   const remove = taskList.splice(startIndex, 1)
   taskList.splice(endIndex, 0, remove[0])
 }
 
-export const Tasks = (props) => {
+export const Tasks: VFC<Props> = (props: { taskList: any; setTaskList: any; }) => {
   const { taskList, setTaskList } = props;
 
-  const handleDragEnd = (result) => {
+  const handleDragEnd = (result: { source: { index: any; }; destination: { index: any; }; dest: any; }) => {
     reorder(taskList, result.source.index, result.destination.index);
     setTaskList(taskList, result.source.index, result.dest)
   }
@@ -29,7 +29,7 @@ export const Tasks = (props) => {
         <Droppable droppableId='droppable'>
           {(provided) =>
             <div {...provided.droppableProps} ref={provided.innerRef}>
-              {taskList.map((task, index) => (
+              {taskList.map((task: { id: number; text: string; draggableId: string; }, index: number) => (
                 <div key={task.id}>
                   <Task index={index} task={task} taskList={taskList} setTaskList={setTaskList} />
                 </div>
